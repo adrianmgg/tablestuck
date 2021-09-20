99a10e79995edd910a704f0b457e4dbd258cf6dc
adrianmgg <2adrian2@gmail.com>
2021-09-20 12:04:15 -0700

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const tourney = true;
+const tourney = false;
 
 module.exports = (client, message) => {
   //ignore all bots
@@ -9,21 +9,29 @@ module.exports = (client, message) => {
   //Ignore all messages not starting with the prefix
   if(message.content.indexOf(client.auth.prefix) !== 0) return;
 
-  var freeAct = ["register","leaderboard","stats","scratch","help","initialize","trait","act"];
+  var freeAct = ["register","leaderboard","stats","scratch","help","initialize","trait"];
 
   if(tourney){
-    freeAct = ["register","leaderboard","stats","scratch","help","initialize","trait","heal","consume","act","strife","switch","specibus","sylladex","captcha","eject","alchemize","armor","trinket","equip","list","inspect","pass","quickalch","grist","ath","say","rename","use","allocate","push"];
+    freeAct = ["register","leaderboard","stats","scratch","help","initialize","trait","heal","consume","act","strife","switch","specibus","sylladex","captcha","eject","alchemize","armor","trinket","equip","list","inspect"];
   }
 
   //standard argument/command name definition
   const args = message.content.slice(client.auth.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  let rawCommand = args.shift();
+  let command = rawCommand.toLowerCase();
   console.log(message.content);
 
+  if(command.startsWith(':')){
+    message.channel.send(message.content);
+    return;
+  }
   // Grab the command data from the client.commands Enmap
   const cmd = client.commands.get(command);
   // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
+  if (!cmd) {
+    message.channel.send(`unknown command "${command}"`);
+	return;
+  };
   try{
     let charid = message.guild.id.concat(message.author.id);
 
@@ -35,17 +43,17 @@ module.exports = (client, message) => {
       }
     }
 
-    if(!reg&&command!="register"&&command!="help"&&command!="initialize"&&command!="scratch"&&command!="leaderboard"&&command!="config"){
+    if(!reg&&command!="register"&&command!="help"&&command!="initialize"&&command!="scratch"&&command!="leaderboard"){
       message.channel.send("You are not registered! Register using the >register command!");
       return;
     }
 
-    if(client.playerMap.get(charid,"act")>=client.limit&&client.limit!=0&&!freeAct.includes(command)&&!client.funcall.dmcheck(client,message)){
+    if(client.playerMap.get(charid,"act")>=client.limit&&client.limit!=0&&!freeAct.includes(command)){
       message.channel.send("You have reached your maximum number of actions for this tournament!");
       return;
     }
   } catch(err){
- console.log("A player took an action when not reigistered, probably the DM. Mostly good to ignore.");
+ console.log("failed when checking to see if someone is registered, or if they're over the tournament action limit.");
   }
   if (client.limit != 0) {
 	 client.user.setActivity("PESTERCORD TOURNAMENT");
